a5f96fa16b8f3e4424ef19776f4fd56bc6976293
leszek-bumbul <l.bumbul@gmail.com>
2019-12-30 00:11:04 +0100

    Simplify Runner class

--- src/main/java/pl/bumbul/adventofcode/edition2019/Edition2019Runner.java
+++ src/main/java/pl/bumbul/adventofcode/edition2019/Edition2019Runner.java
@@ -4,23 +4,21 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 @Log4j2
 public class Edition2019Runner {
 
-    private Task task1;
-    private Task task2;
+    private List<Task> taskList;
 
     public Edition2019Runner(Task task1, Task task2) {
-        this.task1 = task1;
-        this.task2 = task2;
+        this.taskList = List.of(task1, task2);
     }
 
     @PostConstruct
     public void init() {
         log.info("Advent of code - edition 2019");
-        task1.execute();
-        task2.execute();
+        taskList.forEach(Task::execute);
     }
 }
